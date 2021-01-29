f0a0656c4d633015152d295faa3a28484d447865
Maria1709 <G00364700@gmit.ie>
2021-01-29 19:14:22 +0000

    (new data)

---
+++ bmi.py
@@ -0,0 +1,28 @@
+#Here we are writng a program to take information from the user to assertain a bmi result from their input 
+
+height = float(input ("Enter height in metres: "))
+weight = float(input ("Enter weight in in kg: " ))  
+
+# Below is the formula for calulating bmi
+
+bmi = weight/(height**2)
+
+print("Your BMI is: {0} and you are: ".format(bmi), end='')
+
+#conditions
+
+if ( bmi < 16):
+   print("obese")
+
+elif ( bmi >= 16 and bmi < 18.5):
+   print("underweight")
+
+elif ( bmi >= 18.5 and bmi < 25):
+   print("Healthy weight")
+
+elif ( bmi >= 25 and bmi < 30):
+   print("overweight")
+
+elif ( bmi >=30):
+   print("obese")
+
