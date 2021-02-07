66da0d66eff60b6f586e6cd294dbc6d84f7451ad
Maria1709 <G00364700@gmit.ie>
2021-02-07 17:08:52 +0000

    Update email.py

--- email.py
+++ email.py
@@ -1,6 +1,12 @@
 import smtplib
-server = smtplib.SMTP('smtp.gmail.com',587)
-server.starttls()
-server.login(username,password)
-server.sendmail(FROM_EMAIL_ADDRESS,TO_EMAIL_ADDRESS,MESSAGE)
-
+import sys
+sys.path.append(C:\Users\maria\OneDrive\Desktop\gmit\email details)
+import details # The password and the email are stored outside of git folder 
+server = smtplib.SMTP_SSL('smtp.gmail.com',587)
+FROM_EMAIL_ADDRESS = input('Who is sending the email?')
+TO_EMAIL_ADDRESS = input('Who are you sending the email to?')
+SUBJECT= input("Enter the subject of the email")
+TEXT = input('Write your message')
+MESSAGE = 'Subject: {}\n\n{}'.format(SUBJECT, TEXT)
+server.login(details.username,details.password)
+server.sendmail(FROM_EMAIL_ADDRESS,TO_EMAIL_ADDRESS,MESSAGE, SUBJECT)
