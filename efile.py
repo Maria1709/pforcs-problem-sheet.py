38eb94d0a6890dc9217b98254f6abb9d9bfae245
Maria1709 <G00364700@gmit.ie>
2021-02-20 11:56:27 +0000

    Update efile.py

--- efile.py
+++ efile.py
@@ -1,10 +1,11 @@
 # Author: Maria carroll
-# Moby files
+# Moby files 
 
+# open moby text file to read adn then create the file 
 with open("moby.txt", "r+") as f:
     print("create a file")
     moby = f.read ()
 f.close()
-
+# count the frequency of e,s that are in the moby document and print, result is 116960
 freq = moby.count("e")
 print (freq)
