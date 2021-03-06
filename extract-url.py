249dc05feeda67b7bd45e62bfb69cb9d3dff50ad
Maria1709 <G00364700@gmit.ie>
2021-03-06 15:25:05 +0000

    Update extract-url.py

--- extract-url.py
+++ extract-url.py
@@ -3,8 +3,8 @@
 
 import re
 
-#regex = '\w+='
-#regex = '=\w+'
+#regex = '\w+=' the variable names in a urls
+#regex = '=\w+' the variable values in the urls
 regex = '"((http)s?://.*?)"'
 filename = r"C:\Users\maria\OneDrive\Desktop\Programming\pforcs-problem-sheet.py\pforcs-problem-sheet.py\tutorialdata\www1\access.log"
 Output = []
